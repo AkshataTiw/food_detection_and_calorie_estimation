4ac242d431a72e8cbe9d16ddbb85f7224b6a8168
Akshata <akshatatiwari5@gmail.com>
2026-04-21 19:12:56 +0530

    trial2

--- app.py
+++ app.py
@@ -189,15 +189,14 @@ if uploaded:
     col1, col2 = st.columns(2)
 
     with col1:
-        st.image(image, caption="Uploaded Image", width="stretch")
-
+        st.image(image, caption="Uploaded Image", width=500)
     if st.button("🚀 Analyze"):
         with st.spinner("Analyzing..."):
 
             df, total, annotated = predict(image)
 
             with col2:
-                st.image(annotated, caption="Detected", width="stretch")
+                st.image(annotated, caption="Detected", width=500)
 
             st.markdown("### 📊 Results")
             st.dataframe(df)
